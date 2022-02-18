1c6324bc9956ee51160e29b6f7c348f43da4731b
Sérgio Fontes <sergio@fontes.cc>
2022-02-17 22:21:19 -0300

    Add grid to global

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'normalize.css/normalize.css';
 import '../styles/typography.scss';
+import '../styles/grid.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
