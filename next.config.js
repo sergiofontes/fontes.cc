24221f4df1dafd0ff53955691e4a0c0832bc9f81
Sérgio Fontes <sergio@fontes.cc>
2022-03-07 21:29:35 -0300

    Add locale to main HTML tag

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
