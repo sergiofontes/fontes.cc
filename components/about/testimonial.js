5627814d95fda1d2b202b51416daad8baf20c6ed
Sérgio Fontes <sergio@fontes.cc>
2026-06-24 15:14:59 -0300

    Use curly apostrophes in the testimonial

--- components/about/testimonial.js
+++ components/about/testimonial.js
@@ -5,13 +5,13 @@ export default function Experience() {
     <figure id="testimonial" className="testimonial grid content">
       <blockquote>
         <p className="lead">
-          “Simply put, Sérgio is one of the best designers I've ever worked
+          “Simply put, Sérgio is one of the best designers I’ve ever worked
           with. His passion for design research, process and even front-end
-          development can raise up the quality of any project he's involved
+          development can raise up the quality of any project he’s involved
           with. His motivation can be contagious, pushing the team to do their
           best and reach beyond that. Projects under his umbrella are sure to
-          reach their goals. We've had a long and productive partnership both at
-          Guava and VTEX and I wouldn't blink an eye before recommending him.”
+          reach their goals. We’ve had a long and productive partnership both at
+          Guava and VTEX and I wouldn’t blink an eye before recommending him.”
         </p>
       </blockquote>
       <figcaption>
